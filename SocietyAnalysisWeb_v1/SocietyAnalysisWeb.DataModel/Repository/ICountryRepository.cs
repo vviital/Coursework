34c4b897f2414a453e82969815007be435630163
vviital <vviital@gmail.com>
2015-05-26 21:00:18 +0400

    Repositories

---
+++ SocietyAnalysisWeb_v1/SocietyAnalysisWeb.DataModel/Repository/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocietyAnalysisWeb.DataModel.Repository
+{
+    public interface ICountryRepository : IRepository<Country>
+    {
+
+    }
+}
---
+++ SocietyAnalysisWeb_v1/SocietyAnalysisWeb.DataModel/Repository/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocietyAnalysisWeb.DataModel.Repository
+{
+    public interface IRepository<TEntity> 
+        where TEntity : class
+    {
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Delete(Expression<Func<TEntity, bool>> where);
+
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where);
+
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        
+        int Count(Expression<Func<TEntity, bool>> where = null);
+
+        bool IsExist(Expression<Func<TEntity, bool>> where = null);
+
+        void Save();
+
+        void Dispose();
+    }
+}
